ffc833fc8c1efa8b514e429d1c645406dcb4003b
Faruk Pektas <fpektas@k1m41s04.42istanbul.com.tr>
2024-01-22 04:23:05 +0300

    .

--- srcs/requirements/nginx/tools/nginx_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-echo "Nginx: setting up ssl ...";
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=TR/ST=ISTANBUL/L=SARIYER/O=wordpress/CN=fpektas.42.fr";
-echo "Nginx: ssl is set up!";
-fi
-
-exec "$@"
